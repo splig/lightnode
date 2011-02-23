c29445455332feebfd36be5d29b63c170e08d0ef
craigsg <craigsg@gmail.com>
2011-02-23 06:23:30 -0800

    Code cleaning

--- examples/delegation/serve-host-with-api.js
+++ examples/delegation/serve-host-with-api.js
@@ -1,13 +1,30 @@
-
-var http = require('http')
-var lightnode = require('../lightnode')
+var PORT      = 8888;
+var http      = require('http');
+var lightnode = require('../lightnode');
 
 
 // 1 - create and start the node ip server
-var server = new http.Server(); server.listen(8081)
+var server = new http.Server(); server.listen(8888);
 		
 // 2 - create the file server for the root directory
-var website = new lightnode.FileServer('/home/web/www.ngspinners.com')
+var website = new lightnode.FileServer('/home/web/www.ngspinners.com');
+
+
+
+//this is run when the given request must be handled by the API
+function apiHandler(req, resp) {
+  // I'm a hello world API
+  var message = 'Hello World!';
+  resp.writeHead(200, {
+    'content-type': 'text/plain',
+    'content-length': message.length
+  });
+  resp.write(message);
+  resp.end();
+}
+
+
+
 
 // before any requests come in
 
@@ -16,33 +33,16 @@ website.delegateRequest = function(req, resp) {
 
 	// 3 - requests starting with path '/api' are sent to the api server
 	
-	if (req.url.indexOf('/api') == 0)
-		return apiHandler
-		
+	if (req.url.indexOf('/api') === 0) { return apiHandler; }
 	// 4 - serve all other requests with this file server object
-	
-	else
-		return website
-
-}
+	else { return website; }
+};
 
 // when a request comes to the ip server
 server.addListener('request', function(req, resp) {
-	
-	// pass all requests to the file server
-	website.receiveRequest(req, resp)
-	
-})
+		// pass all requests to the file server
+	website.receiveRequest(req, resp);
+});
 
 
-// this is run when the given request must be handled by the API
-function apiHandler(req, resp) {
-	// I'm a hello world API
-	var message = 'hello, world.'
-	resp.writeHead(200, {
-		'content-type': 'text/plain',
-		'content-length': message.length
-	})
-	resp.write(message)
-	resp.end()
-}
+
