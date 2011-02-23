a7680ede2a503cf2bdd3d42e6ca1bf382f95459f
craigsg <craigsg@gmail.com>
2011-02-23 00:36:29 -0800

    Edited examples/serve-host.js via GitHub

--- examples/serve-host.js
+++ examples/serve-host.js
@@ -1,22 +1,24 @@
 
+var PORT = 8888;
+
 // the nodeJS http library
-var http = require('http')
+var http = require('http');
 
 // the lightnode library, usually required just as 'lightnode'
-var lightnode = require('../lightnode')
+var lightnode = require('../lightnode');
 
 
 // 1 - create and start the nodeJS ip server object
-var server = new http.Server(); server.listen(8081)
+var server = new http.Server(); server.listen(PORT);
 		
 // 2 - create the lightnode file server for the root directory
-var website = new lightnode.FileServer('/home/web/www.ngspinners.com')
+var website = new lightnode.FileServer('/home/web/www.ngspinners.com');
 
 // when a request comes to the ip server
 server.addListener('request', function(req, resp) {
 	
 	// 3 - serve all requests with the directory server
 	
-	website.receiveRequest(req, resp)
+	website.receiveRequest(req, resp);
 
-})
+});
